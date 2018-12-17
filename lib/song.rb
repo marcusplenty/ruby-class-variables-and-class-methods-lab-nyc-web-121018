e6ecc011aca3e0fecfe8cc05980e032a9be10ec6
Marcus Plenty <marcusplenty@gmail.com>
2018-12-17 03:54:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
   @@count = 0
   @@genres = []
+  @@artists = []
   attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
     @name = name
@@ -8,6 +9,7 @@ class Song
     @genre = genre
     @@count +=1
     @@genres << genre
+    @@artists << artist
   end 
   def self.count
     @@count
@@ -15,5 +17,21 @@ class Song
   
   def self.genres
     @@genres.uniq!
+  end 
+  
+  def self.artists
+    @@artist.uniq!
+  end
+  
+  def self.genre_count
+    gen_hash = {}
+    @@genres.each do |genre|
+      if gen_hash.include?(genre)
+        gen_hash[genre] += 1
+      else
+        gen_hash[genre] = 1
+      end 
+    end
+    return gen_hash
   end 
 end 
