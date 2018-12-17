c8d2b5516042e4d9bda4c02a6a67286beff89aee
Marcus Plenty <marcusplenty@gmail.com>
2018-12-17 03:48:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,19 @@
 class Song
+  @@count = 0
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genre << genre
+  end 
+  def self.count
+    @@count
+  end
   
+  def self.genres
+    @@genres.uniq!
+  end 
 end 
